48a9f84034854e096dc9e3135c67e95313541f52
allison <allison.villa@outlook.com>
2022-05-29 09:59:53 -0400

    moved entries component into EntriesView instead

--- src/components/Submit.vue
+++ src/components/Submit.vue
@@ -4,52 +4,58 @@ import { ref, set } from 'firebase/database';
 import { reactive } from 'vue';
 
 // Reactive variable for user's input
-const userInput = reactive({ entry: '' }); 
+const userInput = reactive({ entry: '' });
 
 // Event handler for submitting entry to database
 const handleSubmit = (event) => {
-    event.preventDefault();
+	event.preventDefault();
 
-    // Create a post ID using current time
-    const postID = Date.now().toString();
+	// Create a post ID using current time
+	const postID = Date.now().toString();
 
-    // Store current date and time
-    const dateFormat = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        timeZone: 'Canada/Eastern',
-        timeZoneName: 'short',
-        hour: 'numeric',
-        minute: 'numeric',
-    };
+	// Store current date and time in timestamp variable
+	const dateFormat = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		timeZone: 'Canada/Eastern',
+		timeZoneName: 'short',
+		hour: 'numeric',
+		minute: 'numeric',
+	};
 
-    const timestamp = new Intl.DateTimeFormat('en-CA', dateFormat).format(Date.now()); 
+	const timestamp = new Intl.DateTimeFormat('en-CA', dateFormat).format(
+		Date.now()
+	);
 
-    // Push user entry to Firebase
-    set(ref(database, postID), {
-        entry: userInput.entry,
-        time: timestamp, 
-        // Initialize likes count as 0 
-        likes: 0
-    });
+	// Push user entry to Firebase
+	set(ref(database, postID), {
+		entry: userInput.entry,
+		time: timestamp,
+		// Initialize likes count as 0
+		likes: 0,
+	});
 
-    // Reset user input
-    userInput.entry = ''; 
-}
-
+	// Reset user input
+	userInput.entry = '';
+};
 </script>
 
 <template>
-    <div class="submit-form">
-        <form action="">
-            <label htmlFor="submission">What are you grateful for today?</label>
-            <input type="textarea" name="submission" id="submission" placeholder="Today, I'm grateful for..." v-model="userInput.entry"/>
-            <button @click="handleSubmit">Submit</button>
-        </form>
-    </div>
+	<div class="submit-form">
+		<form action="">
+			<label htmlFor="submission">What are you grateful for today?</label>
+			<input
+				type="textarea"
+				name="submission"
+				id="submission"
+				placeholder="Today, I'm grateful for..."
+				v-model="userInput.entry"
+			/>
+			<button @click="handleSubmit">Submit</button>
+		</form>
+	</div>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/views/EntriesView.vue
+++ src/views/EntriesView.vue
@@ -1,10 +1,37 @@
 <script setup>
-import Entries from '../components/Entries.vue';
+import { ref, onValue } from 'firebase/database';
+import { reactive } from 'vue';
+import database from '@/firebaseConfig.js';
+import Entry from '@/components/Entry.vue';
+
+// Reactive variable that will hold the entries returned from Firebase
+const entries = reactive([]);
+
+// Make reference to our database
+const dbRef = ref(database);
+
+// When an entry is added by a user or a likes count is updated, update the entries variable
+onValue(dbRef, (response) => {
+	// Clear entries array to avoid duplicate renders on like
+	entries.splice(0);
+
+	// Store the response in a variable
+	const data = response.val();
+
+	for (let key in data) {
+		entries.push({
+			id: key,
+			entry: data[key].entry,
+			time: data[key].time,
+			likes: data[key].likes,
+		});
+	}
+});
 </script>
 
 <template>
-    <div class="about">
-        <h1>More reasons to be grateful</h1>
-        <Entries />
-    </div>
+	<section class="entries">
+		<h1>More reasons to be grateful</h1>
+		<Entry v-for="item in entries" v-bind="item" />
+	</section>
 </template>
